1a2d5923c41b05539a1e1541020256ebdc1e01f9
venkatkadiri <venkatkadiri@hotmail.com>
2020-12-31 20:55:21 +0530

    Faq Section

---
+++ src/components/Faq.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Faq() {
+    return (
+        <div className="faq">
+            <h2>My <span>Information</span></h2>
+            <div className="Question">
+                <h4>What is My Profession</h4>
+                <div className="Answer">
+                    <p>I am A Full Stack Developer</p>
+                </div>
+            </div>
+            <div className="Question">
+                <h4>How Many Years of Expiriance Do I Have</h4>
+                <div className="Answer">
+                    <p>13 Months</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Faq
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../components/About";
 import Skills from "../components/Skills";
+import Faq from "../components/Faq";
 
 function AboutMe() {
   return (
     <>
       <About />
       <Skills />
+      <Faq/>
     </>
   );
 }

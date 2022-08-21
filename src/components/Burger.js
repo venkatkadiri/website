7812523ab43a3066b113ccd7b4c20b2d999078ef
venkat kadiri <venkatkadiri@hotmail.com>
2022-08-21 20:03:23 +0530

    Update Burger.js

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,40 +1,81 @@
-import React from "react";
+import React from 'react';
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import '../styles/app.style.scss';
-import { pageAnimation } from "../animation";
-import { motion } from "framer-motion";
+import { useState } from 'react';
+import BurgerContents from './BurgerContents';
 
 
 
-function BurgerContents({selected}) {
-    const selectedHandler = selected;
-    console.log(selectedHandler);
+function BurgerMenu() {
+  const [clicked, setClicked] = useState(false); //burger clicked or not...
+  const burgerClickHandler = () =>{
+      
+    setClicked(!clicked);
+
+    
+  }
+
   return (
-    <motion.div
-      variants={pageAnimation}
-      initial="hidden"
-      animate="show"
-      exit="exit"
-     className="burger_contents">
-      <ul>
-        <li>
-          <Link to="/" onClick={selectedHandler}>About Me</Link>
-        </li>
-        <li>
-          <Link to="/projects" onClick={selectedHandler}>My Projects</Link>
-        </li>
-        <li>
-          <Link to="/blog" onClick={selectedHandler} >My Blog</Link>
-        </li>
-        <li>
-          <Link to="/contact" onClick={selectedHandler} >Contact Me</Link>
-        </li>
-      </ul>
-    </motion.div>
+      <>
+      <span className = {clicked ? 'background-open' : 'background'}></span>
+      <StyledBurger>
+        
+      <h1>
+        <Link id="Logo" to="/">
+          Venkat
+        </Link>
+      </h1>
+      <Burger onClick={burgerClickHandler} className={clicked ? 'b_menu' : ''}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </Burger>
+      </StyledBurger>
+      {(clicked)?(<BurgerContents selected = {burgerClickHandler} />):(<></>)}
+     </>
   )
 }
+const StyledBurger = styled.nav`
+ display:flex;
+ justify-content:space-between ;
+  align-items: center;
+  min-height: 10vh;
+  padding: 1rem 1rem;
+  background-color: #282828;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+a {
+    color: white;
+    text-decoration: none;
+  }
+#Logo {
+    font-family: Lobster Two;
+    font-size: 1.8rem;
+    font-weight: lighter;
+  }  
+ 
+`;
 
+const Burger = styled.ul `
+cursor: pointer;
 
+span{
+    display:block;
+    width:26px;
+    height:2px;
+    background-color:white;
+    transition: all 300ms ease-in-out;
+    transform-origin:left center ;
+    color: white ;
 
-export default BurgerContents
+    &:not(:last-child){
+        margin-bottom: 3px;
+    }
+    
+}
+`;
 
+export default BurgerMenu;
